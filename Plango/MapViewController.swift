f63fbefdb840a40fd0ba852683ae74d2d03bbb08
Douglas Hewitt <doughewitt4@gmail.com>
2016-07-14 16:56:13 -0700

    basic user location and navigation implemented

--- Plango/MapViewController.swift
+++ Plango/MapViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import Mapbox
+import MapboxDirections
 
 class MapViewController: UIViewController, MGLMapViewDelegate {
     
     var mapView: MGLMapView!
     var experiences: [Experience]?
     private var places: [MGLPointAnnotation]!
+    
+    let directions = Directions.sharedDirections
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +24,8 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
         mapView = MGLMapView(frame: self.view.bounds)
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         mapView.delegate = self
-        
+        mapView.showsUserLocation = true
+
         if let userLocation = mapView.userLocation {
             mapView.setCenterCoordinate(userLocation.coordinate, zoomLevel: 14, animated: false)
         }
@@ -73,16 +77,74 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
     func mapView(mapView: MGLMapView, imageForAnnotation annotation: MGLAnnotation) -> MGLAnnotationImage? {
         return nil
     }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        mapView.showsUserLocation = true
+    
+    
+//    func mapViewWillStartLocatingUser(mapView: MGLMapView) {
+//        print("lat: \(mapView.userLocation?.coordinate.latitude) long: \(mapView.userLocation?.coordinate.longitude)")
+//    }
+    
+    func mapView(mapView: MGLMapView, didFailToLocateUserWithError error: NSError) {
+        printError(error)
     }
+    
     
     func mapViewDidFinishLoadingMap(mapView: MGLMapView) {
         if places?.count == 1 {
             mapView.selectAnnotation(places!.first!, animated: true)
         }
+    }
+    
+    func mapView(mapView: MGLMapView, tapOnCalloutForAnnotation annotation: MGLAnnotation) {
+        
+        guard let userCoordinates = mapView.userLocation?.coordinate else {return}
+        
+        let waypoints = [
+            Waypoint(coordinate: userCoordinates),
+            Waypoint(coordinate: annotation.coordinate),]
+        
+        let options = RouteOptions(waypoints: waypoints, profileIdentifier: MBDirectionsProfileIdentifierAutomobile)
+        options.includesSteps = true
+        
+        directions.calculateDirections(options: options) { (waypoints, routes, error) in
+            guard error == nil else {
+                print("Error calculating directions: \(error!)")
+                return
+            }
+            
+            if let route = routes?.first, leg = route.legs.first {
+                print("Route via \(leg):")
+                
+                let distanceFormatter = NSLengthFormatter()
+                let formattedDistance = distanceFormatter.stringFromMeters(route.distance)
+                
+                let travelTimeFormatter = NSDateComponentsFormatter()
+                travelTimeFormatter.unitsStyle = .Short
+                let formattedTravelTime = travelTimeFormatter.stringFromTimeInterval(route.expectedTravelTime)
+                
+                print("Distance: \(formattedDistance); ETA: \(formattedTravelTime!)")
+                
+                for step in leg.steps {
+                    print("\(step.instructions)")
+                    let formattedDistance = distanceFormatter.stringFromMeters(step.distance)
+                    print("— \(formattedDistance) —")
+                }
+                
+                if route.coordinateCount > 0 {
+                    // Convert the route’s coordinates into a polyline.
+                    var routeCoordinates = route.coordinates!
+                    let routeLine = MGLPolyline(coordinates: &routeCoordinates, count: route.coordinateCount)
+                    
+                    // Add the polyline to the map and fit the viewport to the polyline.
+                    mapView.addAnnotation(routeLine)
+                    
+                    let margins = UIEdgeInsetsMake(24, 24, 24, 24)
+                    
+                    mapView.setVisibleCoordinates(&routeCoordinates, count: route.coordinateCount, edgePadding: margins, animated: true)
+                }
+            }
+        }
+        
+        
     }
 
 }
